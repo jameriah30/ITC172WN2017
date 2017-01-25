3f2dd5ad9b4de02921a83a03f19f152eb1d7cde3
Jeremiah Smith <jsmit028@seattlecentral.edu>
2017-01-25 12:16:25 -0800

    Add files via upload

---
+++ Assignment3Example/App_Code/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data; //general data tools
+using System.Data.SqlClient;//sql server data tools
+using System.Configuration;//tools for talking to the config file
+
+/// <summary>
+/// Summary description for  login Class1
+/// </summary>
+public class Class1
+{
+    SqlConnection connect;
+    string user;
+    string pass;
+    public Class1(string username, string password)
+    {
+        user = username;
+        pass = password;
+        string connectString = ConfigurationManager.ConnectionStrings["BookReviewConnection"].ToString();
+
+        connect = new SqlConnection(connectString);
+       
+    }
+    public int ValidateLogin()
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = connect;
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandText = "usp_ReviewerLogin";
+        cmd.Parameters.AddWithValue("@ReviewerUserName", user);
+        cmd.Parameters.AddWithValue("@ReviewerPassword", pass);
+        connect.Open();
+        int result = cmd.ExecuteNonQuery();
+        connect.Close();
+
+        int key = 0;
+        if(result != -1)
+        {
+            key = GetUserKey();
+        }
+        return key;
+    }
+
+    private int GetUserKey()
+    {
+        string sql = "Select ReviewerKey from reviewer where ReviewerUserName =@user";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        cmd.Parameters.AddWithValue("@user", user);
+        SqlDataReader reader = null;
+        connect.Open();
+        reader = cmd.ExecuteReader();
+        int key = 0;
+        while (reader.Read())
+        {
+            key = (int)reader["ReviewerKey"];
+        }
+        connect.Close();
+
+        return key;
+
+    }
+}
---
+++ Assignment3Example/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void LoginButton_Click(object sender, EventArgs e)
+    {
+        Class1 lc = new Class1(UserTextBox.Text, PasswordTextBox.Text);
+        int key = lc.ValidateLogin();
+        if(key!=0)
+        {
+            ResultLabel.Text = "Welcome";
+
+        }
+        else
+        {
+            ResultLabel.Text = "Invalid Login";
+        }
+    }
+}
