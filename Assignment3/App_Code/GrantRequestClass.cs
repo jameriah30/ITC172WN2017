c4a9c88dcb74535b396ffc77ba2132a1f382300b
Jeremiah Smith <jsmit028@seattlecentral.edu>
2017-02-01 12:17:53 -0800

    Add files via upload

---
+++ Assignment3/App_Code/GrantRequestClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data; //these libraries must be added
+using System.Data.SqlClient; //sql server tools
+using System.Configuration; //to talk to the config file
+
+/// <summary>
+/// Summary description for GrantRequestClass
+/// </summary>
+public class GrantRequestClass
+{
+    //declare the connection string
+    private SqlConnection connect;
+    public GrantRequestClass()
+    {
+        //get the connection string
+        string connectString =
+            ConfigurationManager.ConnectionStrings["GrantReviewConnection"].ToString();
+        //initilize the connection object
+        connect = new SqlConnection(connectString);
+    }
+    //this method takes a parameter authorkey
+    //and returns all the books for that author
+    public DataTable GetGrants(int GrantTypeKey)
+    {
+        //declare a new data table to store the results
+        DataTable table = new DataTable();
+        //sql statement
+        string sql = "Select GrantReviewDate as [Date], "
+             + "GrantRequestExplanation as Explanation, "
+            + "GrantAllocationAmount as Amount, "
+            + "GrantRequestStatus as [Status] "
+            + "From GrantRequest r "
+            + "inner join GrantReview gr "
+            + "on r.GrantRequestKey = gr.GrantRequestKey "
+            + " Where GrantTypeKey = @GrantTypeKey";
+        //the command object passes the sql through the connection
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        //the parameter replaces the sql variable with a value
+        cmd.Parameters.AddWithValue("@GrantTypeKey", GrantTypeKey);
+        //declare a reader to stream the results
+        SqlDataReader reader = null;
+        //open the connection
+        connect.Open();
+        //execute the command and reader
+        reader = cmd.ExecuteReader();
+        //load the results into the table 
+        table.Load(reader);
+        //close stuff
+        reader.Close();
+        connect.Close();
+        return table;
+    }
+
+    public DataTable GrantExplanation()
+    {
+        DataTable table = new DataTable();
+        string sql = "Select GrantTypeKey, GrantTypeName from GrantType";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        SqlDataReader reader = null;
+        connect.Open();
+        reader = cmd.ExecuteReader();
+        table.Load(reader);
+        reader.Close();
+        connect.Close();
+        return table;
+    }
+}
